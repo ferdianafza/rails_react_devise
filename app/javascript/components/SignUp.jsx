2d481de18eb457d5faa521d8903d94b99e671207
Ferdian <farisafza201002@gmail.com>
2020-03-07 17:41:41 +0700

    add create acount

--- app/javascript/components/SignUp.jsx
+++ app/javascript/components/SignUp.jsx
@@ -2,39 +2,116 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class SignUp extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.handleSignup = this.handleSignup.bind(this);
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
   }
 
-  handleSignup(e) {
-    e.preventDefault();
-    let that = this
-    axios.post('/users', {
-      user: {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
-        password_confirmation: document.getElementById("password_confirmation").value
-      }
+
+  stripHtmlEntities(str) {
+    return String(str)
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const url = "/api/v1/users";
+    const { email, password, password_confirmation } = this.state;
+
+    if (email.length == 0 || password.length == 0 || password_confirmation.length == 0)
+      return;
+
+    const body = {
+      email,
+      password,
+      password_confirmation: password_confirmation.replace(/\n/g, "<br> <br>")
+    };
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
     })
-    .then(function(response){
-      that.props.changePage("delete");
-      that.props.updateCurrentUser(email);
-    })
-    .catch(function(error){
-      console.log(error)
-    })}render() {
-  return (
-      <div>
-        <h2>Signup</h2>
-        <form>
-          <input id="email" placeholder="email"/>
-          <input id="password" placeholder="password"/>
-          <input id="password_confirmation" placeholder="retype password"/>
-          <button onClick={this.handleSignup}>Submit</button>
-        </form>
-        <button onClick={() => this.props.changePage("login")}>Back to Login</button>
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.props.history.push(`/student`))
+      .catch(error => console.log(error.message));
+  }
+
+ render() {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 offset-lg-3">
+            <h1 className="font-weight-normal mb-5">
+              Add a new student to our awesome student collection.
+            </h1>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="recipeName">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="recipeName"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="recipeIngredients">Password</label>
+                <input
+                  type="text"
+                  name="password"
+                  id="recipeIngredients"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+                <small id="ingredientsHelp" className="form-text text-muted">
+                  Separate each ingredient with a comma.
+                </small>
+              </div>
+              <label htmlFor="instruction">Password Confirmation</label>
+              <input
+                className="form-control"
+                id="instruction"
+                name="password_confirmation"
+                type="text"
+                required
+                onChange={this.onChange}
+              />
+              <button type="submit" className="btn custom-button mt-3">
+                Create Account
+              </button>
+              <Link to="/signin" className="btn btn-link mt-3">
+                Sign in
+              </Link>
+            </form>
+          </div>
+        </div>
       </div>
     );
-  };
-};
+  } 
+}
